27c08dffec64510acb7efd99c357e403c10eeb8c
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-10-25 15:52:17 +0100

    share icon trial
    copy link to clipboard. useless feature in this context

--- Styles/index.js
+++ Styles/index.js
@@ -3,3 +3,14 @@ const navToggle = document.querySelector('.nav-toggle');
 navToggle.addEventListener ('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text)
+  .then(() => {
+    console.log(`Copied text to clipboard: ${text}`);
+    alert(`The link to this page has been copied!`);
+  })
+  .catch((error) => {
+    console.error(`Could not copy text: ${error}`);
+  });
+  }
